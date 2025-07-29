0ba419c23325abd651fdc30f9d991fd30b340cf2
x-FK-x <159540593+x-FK-x@users.noreply.github.com>
2025-07-29 11:30:33 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -105,7 +105,7 @@ echo "$REPO_VERSION" > "$LOCAL_DEV_FILE"
 
 whiptail --title "1002xTOOLS Updater" --msgbox "Update completed successfully to version $REPO_VERSION." 10 50
 
-rm -rf "$TMP_DIR"
+#rm -rf "$TMP_DIR"
 rm "$SCRIPT_DIR/LICENSE"
 
 
