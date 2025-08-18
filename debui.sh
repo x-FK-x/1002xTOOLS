998f0ba2503a4b57423f533ce30f35a569f8c206
x-FK-x <159540593+x-FK-x@users.noreply.github.com>
2025-08-18 10:22:22 +0200

    Update debui.sh

--- debui.sh
+++ debui.sh
@@ -8,8 +8,6 @@ if [[ "$SCRIPT_DIR" == *"/godos"* ]]; then
   VERSION="godos"
 elif [[ "$SCRIPT_DIR" == *"/modos"* ]]; then
   VERSION="modos"
-elif [[ "$SCRIPT_DIR" == *"/sodos"* ]]; then
-  VERSION="sodos"
 elif [[ "$SCRIPT_DIR" == *"/wodos"* ]]; then
   VERSION="wodos"
 else
