9fd05995d25adb480b1f99e854b51e88597910a3
x-FK-x <159540593+x-FK-x@users.noreply.github.com>
2025-08-19 13:23:56 +0200

    Update os.sh

--- os.sh
+++ os.sh
@@ -6,25 +6,31 @@ set -e
 # Ziel-Festplatte
 DISK="/dev/sda"
 
-# Partitionen
-ROOT_PART="${DISK}1"
-SWAP_PART="${DISK}2"
-HOME_PART="${DISK}3"
-
 # 1. Festplatte vollständig löschen (alle Partitionen entfernen)
 echo "Lösche alle Partitionen auf ${DISK}..."
 sudo parted ${DISK} --script mklabel gpt
 
 # 2. Partitionierung mit parted (GPT)
 echo "Erstelle Partitionen auf ${DISK}..."
-# Root-Partition: 50% der Festplatte
-sudo parted ${DISK} --script mkpart primary ext4 1MiB 50%
 
-# Swap-Partition: 2 GB
-sudo parted ${DISK} --script mkpart primary linux-swap 50% 52GiB
+# Root-Partition: Erste Partition (größter Teil der Festplatte)
+echo "Erstelle Root-Partition..."
+sudo parted ${DISK} --script mkpart primary ext4 1MiB 100%
 
-# Home-Partition: Rest des Speicherplatzes
-sudo parted ${DISK} --script mkpart primary ext4 52GiB 100%
+# Hole die Gesamtgröße der Festplatte
+DISK_SIZE=$(sudo parted ${DISK} unit GiB print | grep "Disk ${DISK}" | awk '{print $3}' | sed 's/GiB//')
+
+# Berechne die Größe der Swap-Partition (z. B. 4 GB oder 50% der Festplatte, wenn sie weniger als 4 GB groß ist)
+SWAP_SIZE=$((DISK_SIZE < 4 ? 2 : 4))  # Falls die Festplatte kleiner als 4 GB ist, wird die Swap-Partition auf 2 GB gesetzt
+
+# Berechne den verbleibenden Platz für die Home-Partition
+ROOT_PART="${DISK}1"
+SWAP_PART="${DISK}2"
+HOME_PART="${DISK}3"
+
+# Erstelle eine Swap-Partition
+echo "Erstelle Swap-Partition von ${SWAP_SIZE} GB..."
+sudo parted ${DISK} --script mkpart primary linux-swap 100% 100%
 
 # Aktualisiere die Partitionstabelle
 sudo partprobe ${DISK}
@@ -37,16 +43,9 @@ echo "Formatiere Swap-Partition (${SWAP_PART})..."
 sudo mkswap ${SWAP_PART}
 sudo swapon ${SWAP_PART}
 
-echo "Formatiere Home-Partition (${HOME_PART})..."
-sudo mkfs.ext4 ${HOME_PART}
-
 # 4. Mounten der Partitionen
 echo "Mounten der Root-Partition (${ROOT_PART}) auf /mnt..."
 sudo mount ${ROOT_PART} /mnt
-
-echo "Mounten der Home-Partition (${HOME_PART}) auf /mnt/home..."
-sudo mkdir -p /mnt/home
-sudo mount ${HOME_PART} /mnt/home
 
 # 5. Basissystem installieren (Debian/Ubuntu basierte Systeme)
 echo "Installiere Basispakete auf /mnt..."
@@ -80,4 +79,3 @@ sudo umount -R /mnt
 
 # 11. Fertig! Das System ist nun installiert.
 echo "Fertig! Dein System ist jetzt installiert. Du kannst jetzt von der Festplatte starten."
-
