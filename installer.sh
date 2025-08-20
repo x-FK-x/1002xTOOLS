dce99e299edfde976591d86f985fcbd21415d288
x-FK-x <159540593+x-FK-x@users.noreply.github.com>
2025-08-20 14:44:56 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -85,17 +85,11 @@ fi
 while true; do
   ACTION=$(whiptail --title "Installer finished" --menu "What do you want to do now?" 10 50 2 \
     "1" "Return to main menu" \
-    "2" "Exit 1002xTOOLStools" 3>&1 1>&2 2>&3)
+    "2" "Exit 1002xTOOLS" 3>&1 1>&2 2>&3)
 
   case "$ACTION" in
     "1")
-      PARENT_DIR=$(dirname "$SCRIPT_DIR")
-      if [[ -x "$PARENT_DIR/debui.sh" ]]; then
-        exec "$PARENT_DIR/debui.sh"
-      else
-        whiptail --msgbox "Main menu script debui.sh not found or not executable!" 10 50
-        exit 1
-      fi
+      bash /etc/wodos/debui.sh
       ;;
     "2")
       exit 0
