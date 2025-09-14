0ddfa5e8890a2199dff8fefa2659ae3ad0aa9115
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2025-09-14 16:13:42 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -30,16 +30,19 @@ fi
 
 
 OS_FILE="$SCRIPT_DIR/tools/osversion.txt"
-OS_VERSION=$(head -n1 "$OS_FILE")
+if [[ -f "$OS_FILE" ]]; then
+    OS_VERSION=$(head -n1 "$OS_FILE")
+else
+    OS_VERSION="Unknown"
+fi
 
 if [[ "$OS_VERSION" == "1" ]]; then
-  whiptail --title "Updater" --msgbox "You have $VERSION V$OS_VERSION." 10 50
-      :
+    whiptail --title "Updater" --msgbox "You have $VERSION V$OS_VERSION." 10 50
 elif [[ "$OS_VERSION" == "2" ]]; then
     # Vorsorge für OS Version 2 – aktuell nichts zu tun
     :
 else
-    # optional: exit 1
+    echo "OS version unknown: $OS_VERSION"
 fi
 
 
