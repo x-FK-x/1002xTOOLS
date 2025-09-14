911371a580d4ece38bc854457c62f45a5e85c295
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2025-09-14 18:26:12 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -43,7 +43,7 @@ fi
 log "Detected version: $VERSION, SCRIPT_DIR: $SCRIPT_DIR"
 
 # === OS-Version prüfen ===
-OS_FILE="$TARGET_TOOLS_DIR/osversion.txt"
+OS_FILE="$SCRIPT_DIR/tools/osversion.txt"
 if [[ -f "$OS_FILE" ]]; then
     cat $OS_FILE
 else
