38b4c2406765af796b85fb0d845991afd980aaa1
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2025-09-14 18:14:43 +0200

    Aktualisieren von updater.sh

--- updater.sh
+++ updater.sh
@@ -45,7 +45,7 @@ log "Detected version: $VERSION, SCRIPT_DIR: $SCRIPT_DIR"
 # === OS-Version prüfen ===
 OS_FILE="$TARGET_TOOLS_DIR/ostools.txt"
 if [[ -f "$OS_FILE" ]]; then
-    OS_VERSION=$(head -n1 "$OS_FILE")
+    OS_VERSION=cat $OS_FILE
 else
     OS_VERSION="Unknown"
 fi
