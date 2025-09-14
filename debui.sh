97679bde3de096760c8118239e9070a5e778c11f
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2025-09-14 16:46:18 +0200

    Create debui.sh

---
+++ debui.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# === Version Detection ===
+if [[ -d /etc/godos ]]; then
+  VERSION="GODOS"
+  SCRIPT_DIR="/etc/godos"
+elif [[ -d /etc/modos ]]; then
+  VERSION="MODOS"
+  SCRIPT_DIR="/etc/modos"
+elif [[ -d /etc/wodos ]]; then
+  VERSION="VERSION"
+  SCRIPT_DIR="/etc/wodos"
+else
+  whiptail --title "Updater Error" --msgbox "No valid version directory detected. Exiting." 10 50
+  exit 1
+fi
+
+
+
+# === Make all tools executable ===
+chmod +x "$SCRIPT_DIR"/tools/*.sh 2>/dev/null
+chmod -R 777 "$SCRIPT_DIR"/tools/*.sh 2>/dev/null
+
+sudo rm "/etc/resolv.conf"
+sudo cp "$SCRIPT_DIR/tools/resolv.conf" "/etc/resolv.conf"
+sudo cp "$SCRIPT_DIR/tools/motd" "/etc/motd"
+
+
+LOCAL_DEV_FILE="$SCRIPT_DIR/dev.txt"
+LOCAL_VERSION=""
+if [[ -f "$LOCAL_DEV_FILE" ]]; then
+  LOCAL_VERSION=$(head -n1 "$LOCAL_DEV_FILE")
+fi
+
+
+# === Create Desktop Entry ===
+DESKTOP_ENTRY_PATH="/usr/share/applications/1002xTOOLS.desktop"
+if [[ ! -f "$DESKTOP_ENTRY_PATH" ]]; then
+   sudo tee "$DESKTOP_ENTRY_PATH" > /dev/null <<EOF
+[Desktop Entry]
+Name=1002xTOOLS ($VERSION)
+Exec=$SCRIPT_DIR/debui.sh
+Icon=utilities-terminal
+Terminal=true
+Type=Application
+Categories=System;
+EOF
+    sudo chmod +x "$DESKTOP_ENTRY_PATH"
+fi
+
+# === Main Menu ===
+CHOICE=$(whiptail --title "1002xTOOLS Menu ($VERSION VERNO 1.$LOCAL_VERSION)" \
+  --menu "Choose a tool to launch:" 20 60 8 \
+  "1" "Updater of 1002xTOOLS" \
+  "2" "Installer of Software" \
+  "3" "Remover of Software" \
+  "4" "Debian Upgrades" \
+  "5" "Add User" \
+  "6" "Delete User" \
+  "7" "Language Settings" \
+  "8" "Keyboard Manager" \
+  "9" "1002xCMD Installer" \
+  "10" "1002xSUDO Installer" \
+  "11" "Exit" \
+  3>&1 1>&2 2>&3)
+
+case "$CHOICE" in
+  "1") sudo bash "$SCRIPT_DIR/tools/updater.sh" ;;
+  "2") sudo bash "$SCRIPT_DIR/tools/installer.sh" ;;
+  "3") sudo bash "$SCRIPT_DIR/tools/remover.sh" ;;
+  "4") sudo bash "$SCRIPT_DIR/tools/systemupgrade.sh" ;;
+  "5") sudo bash "$SCRIPT_DIR/tools/adduser.sh" ;;
+  "6") sudo bash "$SCRIPT_DIR/tools/deluser.sh" ;;
+  "7") sudo dpkg-reconfigure locales  ;;
+  "8") sudo dpkg-reconfigure keyboard-configuration  ;;
+  "9") sudo bash "$SCRIPT_DIR/tools/1002xCMD-installer.sh" ;; 
+  "10") sudo bash "$SCRIPT_DIR/tools/1002xSUDO-installer.sh" ;; # ← korrekter Dateiname
+  "11"|*) clear; exit ;;
+esac
