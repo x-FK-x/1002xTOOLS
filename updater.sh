8a917ee857de71e935307b6e01339884fb679fe6
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2025-09-19 19:43:25 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -166,6 +166,15 @@ else
     whiptail --title "Updater" --msgbox "osversion.txt not found in folder." 10 50
 fi
 
+# list 
+if [[ -f "$EXTRACTED_DIR/tools/list.txt" ]]; then
+    cp -f "$EXTRACTED_DIR/tools/list.txt" "$SCRIPT_DIR/tools/list.txt"
+    log "Copied list.txt to $SCRIPT_DIR/tools/list.txt"
+else
+    log "osversion.txt not found in folder."
+    whiptail --title "Updater" --msgbox "osversion.txt not found in folder." 10 50
+fi
+
 
 
 
