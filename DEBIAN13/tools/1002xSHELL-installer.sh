25e786203a8eede3b7dc8c09b4f01b9b9f614af8
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-02-11 22:39:38 +0100

    Update 1002xSHELL-installer.sh

--- DEBIAN13/tools/1002xSHELL-installer.sh
+++ DEBIAN13/tools/1002xSHELL-installer.sh
@@ -60,21 +60,21 @@ fi
 EOF
 
 
-sudo sed -i 's/\r$//' /etc/1002xSHELL/v1.sh
+sudo sed -i 's/\r$//' /etc/1002xSHELL/v2.sh
 if grep -q "# 1002xSHELL AUTOLOAD" /etc/bash.bashrc; then
     sudo sed -i '
     /# 1002xSHELL AUTOLOAD/{
         n
-        s|/etc/1002xSHELL/v[0-9]\+\.sh|/etc/1002xSHELL/v1.sh|
+        s|/etc/1002xSHELL/v[0-9]\+\.sh|/etc/1002xSHELL/v2.sh|
         n
-        s|/etc/1002xSHELL/v[0-9]\+\.sh|/etc/1002xSHELL/v1.sh|
+        s|/etc/1002xSHELL/v[0-9]\+\.sh|/etc/1002xSHELL/v2.sh|
     }' /etc/bash.bashrc
 else
     sudo tee -a /etc/bash.bashrc > /dev/null <<'EOF'
 
 # 1002xSHELL AUTOLOAD
-if [[ -f /etc/1002xSHELL/v1.sh ]]; then
-    source /etc/1002xSHELL/v1.sh
+if [[ -f /etc/1002xSHELL/v2.sh ]]; then
+    source /etc/1002xSHELL/v2.sh
 fi
 EOF
 fi
