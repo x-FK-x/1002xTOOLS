06c8d7e16d244d4e63229208bf5722296f842ba5
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-05-08 20:03:46 +0200

    Update updater.sh

--- DEBIAN13/tools/updater.sh
+++ DEBIAN13/tools/updater.sh
@@ -266,11 +266,20 @@ log "Aliases for 1002xTOOLS, 1002xUPDATES and 1002xDNS set in /etc/bash.bashrc"
 # Cleanup
 rm -rf "$TMP_DIR"
 log "Temporary files cleaned."
-rm "$SCRIPT_DIR/tools/LICENSE"
-rm -r "$SCRIPT_DIR/tools/DEBIAN13"
+
 
 whiptail --title "1002xTOOLS Updater" --msgbox "Update completed successfully to version $REPO_VERSION." 10 50
 log "Update completed successfully to version $REPO_VERSION."
+
+
+if [ -f "/etc/profile.d/1002xEASYCOMMAND.sh" ]; then
+   whiptail --title "1002xEASYCOMMAND" --msgbox "1002xEASYCOMMAND is installed. Checking update." 10 50
+   bash "$SCRIPT_DIR/tools/1002xEASYCOMMAND-updater.sh" 
+      whiptail --title "1002xEASYCOMMAND" --msgbox "1002xEASYCOMMAND is installed. Finishing update." 10 50
+      sleep 10
+else
+    whiptail --title "1002xEASYCOMMAND" --msgbox "1002xEASYCOMMAND is not installed. Skipping update." 10 50
+fi
 
 # === Rückkehrmenü ===
 while true; do
