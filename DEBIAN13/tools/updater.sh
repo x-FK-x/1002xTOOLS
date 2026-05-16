2c023f99198e60953b8bd2655f3708b93ff4ff13
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-05-16 21:39:46 +0200

    Update updater.sh

--- DEBIAN13/tools/updater.sh
+++ DEBIAN13/tools/updater.sh
@@ -64,7 +64,7 @@ else
 fi
 #-----
 
-LOCAL_CMD_FILE="$SCRIPT_DIR/tools/1002xSHELL-ver.txt"
+LOCAL_CMD_FILE="$SCRIPT_DIR/tools/1002xCMD-ver.txt"
 REMOTE_URL="https://raw.githubusercontent.com/x-FK-x/1002xCMD/refs/heads/main/version.txt"
 
 if [ -d "/etc/1002xCMD" ]; then
