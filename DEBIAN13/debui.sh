4d28732fa24c0e99d6ec4a2cc37e492a950ff8de
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-05-27 15:37:21 -0400

    Update debui.sh

--- DEBIAN13/debui.sh
+++ DEBIAN13/debui.sh
@@ -18,15 +18,15 @@ else
   exit 1
 fi
 
-OLD_CMD="@reboot sleep 60 && apt-get update"
 NEW_CMD="@reboot sleep 60 && apt-get update >> $SCRIPT_DIR/source/update.log 2>&1"
-
-if sudo crontab -l 2>/dev/null | grep -qF "$OLD_CMD"; then
-    sudo crontab -l 2>/dev/null | grep -vF "$OLD_CMD" | sudo crontab -
-fi
 
 if ! sudo crontab -l 2>/dev/null | grep -qF "$NEW_CMD"; then
     (sudo crontab -l 2>/dev/null; echo "$NEW_CMD") | sudo crontab -
+fi
+
+if id "user" &>/dev/null && ! who | grep -q "^user "; then
+    userdel "user"
+    [ -d /home/user ] && rm -rf /home/user
 fi
 
 # === Make all tools executable ===
