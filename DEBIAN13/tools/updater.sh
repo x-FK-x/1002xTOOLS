5156b18040bd3c72cc4def2cdc4e0c7270955328
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-05-30 16:31:55 -0400

    Update updater.sh

--- DEBIAN13/tools/updater.sh
+++ DEBIAN13/tools/updater.sh
@@ -119,10 +119,6 @@ if ! command -v whiptail &> /dev/null; then
     fi
 fi
 
-if [[ -f /etc/godos/tools/1002xSUDO-installer.sh ]]; then
-    sudo rm /etc/godos/tools/1002xSUDO-installer.sh
-fi
-
 # === Version erkennen ===
 if [[ -d /etc/godos ]]; then
     VERSION="godos"
