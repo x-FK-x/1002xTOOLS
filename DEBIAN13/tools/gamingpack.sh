866ff5346430b3129c3a7d3e5e887ac37bc95bd1
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-06-01 22:03:48 +0200

    Update gamingpack.sh

--- DEBIAN13/tools/gamingpack.sh
+++ DEBIAN13/tools/gamingpack.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+
 if whiptail --title "Gamingpack Installation" --yesno "Do you really want to install the Gamingpack?" 10 50; then
     echo "Starting installation..."
- sudo apt install -y \
-    joystick jstest-gtk antimicrox \
-    xboxdrv steam-devices
+    
+    # Repositories für Wine und Drittanbieter sollten vor "apt install" hinzugefügt werden!
+    sudo apt install -y \
+        joystick jstest-gtk antimicrox \
+        xboxdrv steam-devices
 
-sudo apt install -y lutris steam winehq-stable
+    sudo apt install -y lutris steam winehq-stable
 
-sudo apt install -y gamemode libgamemode0 libgamemodeauto0
+    sudo apt install -y gamemode libgamemode0 libgamemodeauto0
 
-echo "Installation finished. Exiting.."
-sleep 10
-exit
+    echo "Installation finished. Exiting.."
+    sleep 10
+    exit 0
 else
     echo "Installation canceled. Exiting.."
     sleep 10
-    exit 0	
+    exit 0
 fi
-
-
