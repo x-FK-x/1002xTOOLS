a43b7bcf2a15b80d8158bfff1b03141ae27ff70a
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-09-24 21:00:14 +0200

    Aktualisieren von updater.sh

--- DEBIAN13/tools/updater.sh
+++ DEBIAN13/tools/updater.sh
@@ -3,7 +3,7 @@
 clear
 echo "Checking Debian updates"
 sleep 5
-sudo apt update && sudo apt upgrade -y && sudo apt auto remove --purge -y && sudo apt auto clean
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove --purge -y && sudo apt autoclean
 echo "Debian updates finished"
 sleep 7
 clear
