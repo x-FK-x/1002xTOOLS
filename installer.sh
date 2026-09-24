099e02acdf0f0b9e45644c895974b81755f02267
Fabius Karabius <159540593+x-FK-x@users.noreply.github.com>
2026-09-24 21:00:44 +0200

    Aktualisieren von installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo "Success: Admin privileges verified!"
 clear
 echo "Checking Debian updates"
 sleep 5
-sudo apt updates && sudo apt upgrade -y && sudo apt auto remove --purge -y && sudo apt auto clean
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove --purge -y && sudo apt autoclean
 echo "Debian updates finished"
 sleep 7
 clear
